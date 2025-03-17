9562f2402d18dae5dd854f8a3e6957652ff1003f
Giovanni-Marven Alcena <giovanni-marven.alcena@polymtl.ca>
2025-03-17 15:14:39 -0400

    Makefile fini

--- tp/tp7/exec/main.cpp
+++ tp/tp7/exec/main.cpp
@@ -1,6 +1,14 @@
+#include "../lib/button.h"
+#include "../lib/can.h"
+#include "../lib/led.h"
+#include "../lib/memoire_24.h"
+#include "../lib/rs232.h"
+#include "../lib/timer0.h"
+#include "../lib/timer1.h"
+#include "../lib/timer2.h"
 
 int main() {
-
     
+  
     return 0;
 }
