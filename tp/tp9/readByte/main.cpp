a4a3187011a0515219726794f0efe76b7236a9bd
Anis Feteh Benabderrahmane <anis-feteh.benabderrahmane@polymtl.ca>
2025-03-25 02:19:19 -0400

    main.cpp dans bytecode arrangement du code selon guide de codage

--- tp/tp9/readByte/main.cpp
+++ tp/tp9/readByte/main.cpp
@@ -5,28 +5,32 @@
 #include "memoire_24.h"
 #define DELAY_BYTECODE_MS 5
 
-int main() {
+const uint8_t BYTE_SHIFT = 8;
 
-Memoire24CXXX mem;
-uint8_t address = 0x00;
 
-    // Lire le premier octet
-    uint8_t byteShift = 8;
+int main()
+{
+    Memoire24CXXX mem;
+    uint8_t address = 0x00;
     uint8_t addressSize1 = RS232::receiveData();
+
+    // Écritude du premier byte
     mem.ecriture(address, addressSize1);
     _delay_ms(DELAY_BYTECODE_MS);
-    uint16_t realSize_ = addressSize1 << byteShift; // Decalage pour préparer la taille
 
-    
-    // Lire le deuxième octet 
+    uint16_t realSize = addressSize1 << BYTE_SHIFT;
+
+    // Lecture du second byte
     address++;
     addressSize1 = RS232::receiveData();
     mem.ecriture(address, addressSize1);
     _delay_ms(DELAY_BYTECODE_MS);
-    realSize_ |= addressSize1; 
-    
-    // Lire les instructions et les stocker en memoire
-    for (uint16_t j = 2; j < realSize_; j++) {
+
+    realSize |= addressSize1;
+
+    // Lecture des instructions et stockage en mémoire
+    for (uint16_t j = 2; j < realSize; j++)
+    {
         uint8_t buffer = RS232::receiveData();
         mem.ecriture(j, buffer);
         _delay_ms(DELAY_BYTECODE_MS);
