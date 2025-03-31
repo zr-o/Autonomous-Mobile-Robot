0e09b7a6cc878a08b5297e5c578157664c41aa3f
Anis Feteh Benabderrahmane <anis-feteh.benabderrahmane@polymtl.ca>
2025-03-31 11:47:50 -0400

    Modifications des pin du capteur makeline

--- projet/app/line_sensor.cpp
+++ projet/app/line_sensor.cpp
@@ -2,42 +2,38 @@
 
 LineSensor::LineSensor()
 {
-    // Vcc et Gnd
-    Ports::setPinMode(PortMode::WRITE, Port::A, Pin::N1);
-    Ports::setPinMode(PortMode::WRITE, Port::A, Pin::N2);
-    Ports::writePin(Port::A, Pin::N1, true);
-    Ports::writePin(Port::A, Pin::N2, false);
 
+
+    Ports::setPinMode(PortMode::READ, Port::A, Pin::N1);
+    Ports::setPinMode(PortMode::READ, Port::A, Pin::N2);
     Ports::setPinMode(PortMode::READ, Port::A, Pin::N3);
     Ports::setPinMode(PortMode::READ, Port::A, Pin::N4);
     Ports::setPinMode(PortMode::READ, Port::A, Pin::N5);
-    Ports::setPinMode(PortMode::READ, Port::A, Pin::N6);
-    Ports::setPinMode(PortMode::READ, Port::A, Pin::N7);
 }
 
 bool LineSensor::leftDetected()
 {
-    return Ports::readPin(Port::A, Pin::N3);
+    return Ports::readPin(Port::A, Pin::N1);
 }
 
 bool LineSensor::rightDetected()
 {
-    return Ports::readPin(Port::A, Pin::N7);
+    return Ports::readPin(Port::A, Pin::N5);
 }
 
 bool LineSensor::leftMiddleDetected()
 {
-    return Ports::readPin(Port::A, Pin::N4);
+    return Ports::readPin(Port::A, Pin::N2);
 }
 
 bool LineSensor::rightMiddleDetected()
 {
-    return Ports::readPin(Port::A, Pin::N6);
+    return Ports::readPin(Port::A, Pin::N4);
 }
 
 bool LineSensor::middleDetected()
 {
-    return Ports::readPin(Port::A, Pin::N5);
+    return Ports::readPin(Port::A, Pin::N3);
 }
 
 uint8_t LineSensor::sensorsCount()
