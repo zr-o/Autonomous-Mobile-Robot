ec7ca15f00f0e3544fab72fe6ad50c2806760c5d
Giovanni Marven Alcena <giovanni-marven.alcena@polymtl.ca>
2025-03-31 09:17:17 -0400

    test sensorDistance

--- projet/app/main.cpp
+++ projet/app/main.cpp
@@ -1,8 +1,18 @@
 #include "robot.h"
+#include "distanceSensor.h"
+#include "debug.h"
+
+Timer1 sensorTimer = Timer1();
 
 int main()
 {
     Robot& robot = Robot::createRobot();
 
     robot.led().lightUp(Color::GREEN);
+
+    DistanceSensor sensor(sensorTimer);
+
+    uint8_t distance = sensor.readDistance();
+
+    DEBUG_PRINT(distance);
 }
