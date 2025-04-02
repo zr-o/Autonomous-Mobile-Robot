a54423991d81faeab0c2fb15acbbe1b69d83ed4f
Giovanni-Marven Alcena <giovanni-marven.alcena@polymtl.ca>
2025-04-02 16:04:33 -0400

    lettre B et C

--- projet/app/main.cpp
+++ projet/app/main.cpp
@@ -3,7 +3,35 @@
 
 int main()
 {
-    Robot& robot = Robot::createRobot();
+    Robot &robot = Robot::createRobot();
 
-    robot.led().lightUp(Color::GREEN);
+    uint8_t savedDirection[2];
+    uint8_t savedValuesCounter = 0;
+
+    while (savedValuesCounter < 2)
+    {
+
+        if (robot.externButton().isPressed())
+        {
+            savedDirection[savedValuesCounter++] = 1; // 0 pour droite, 1 pour gauche
+            while (robot.externButton().isPressed())
+            {
+                robot.led().lightUp(Color::RED);
+            }
+        }
+
+        if (robot.motherboardButton().isPressed())
+        {
+            savedDirection[savedValuesCounter++] = 0; // 0 pour droite, 1 pour gauche
+            while (robot.motherboardButton().isPressed())
+            {
+                robot.led().lightUp(Color::GREEN);
+            }
+        }
+        robot.led().lightUp(Color::OFF);
+    }
+
+    DEBUG_PRINT(savedDirection[0]);
+    _delay_ms(1000);
+    DEBUG_PRINT(savedDirection[1]);
 }
