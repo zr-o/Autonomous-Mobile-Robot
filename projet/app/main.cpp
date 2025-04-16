75c5317938ea65dd6a1de39702f31c5ad5171510
adazr <adam.azrou@polymtl.ca>
2025-04-16 16:03:35 -0400

    Ajout du lisezmoi.txt et ajout de entete dans le main, fin du projet.

--- projet/app/main.cpp
+++ projet/app/main.cpp
@@ -1,3 +1,18 @@
+/*
+Giovanni Marven Alcena  
+Adam Azrou  
+Elliot Gareau  
+Anis Feteh Benabderrahmane  
+
+Point d'entrée principal du programme.  
+Permet d'exécuter la navigation complète du robot à travers un parcours défini en appelant la méthode doPath().  
+16 avril 2025  
+
+Composantes :  
+Utilisation de la classe Path pour gérer les mouvements et les décisions du robot.  
+(Voir path.h et debug.h pour les définitions et outils de débogage)  
+*/
+
 #include "path.h"
 #include "debug.h"
 
